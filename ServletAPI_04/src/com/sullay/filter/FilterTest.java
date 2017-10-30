b93f9dea88c07c6c90c4cd2eba6153271e896127
sullay <1181518458@qq.com>
2017-10-30 18:41:41 +0800

    filter

---
+++ ServletAPI_04/src/com/sullay/filter/FilterTest.java
@@ -0,0 +1,56 @@
+package com.sullay.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * Servlet Filter implementation class FilterTest
+ */
+@WebFilter(filterName="myfilter",urlPatterns="/Test",initParams={
+	@WebInitParam(name="xxx",value="yyy")	
+})
+public class FilterTest implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public FilterTest() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("filter destroy");
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("filter init"+"   "+fConfig.getInitParameter("xxx"));
+		System.out.println(fConfig.getFilterName());
+	}
+
+}
