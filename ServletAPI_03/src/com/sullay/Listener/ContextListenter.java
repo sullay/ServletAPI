d6d872663fdbe6a5a68eec6dbe48621aa933624f
sullay <1181518458@qq.com>
2017-10-30 15:04:54 +0800

    Listener

---
+++ ServletAPI_03/src/com/sullay/Listener/ContextListenter.java
@@ -0,0 +1,63 @@
+package com.sullay.Listener;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class ContextListenter
+ *
+ */
+@WebListener
+public class ContextListenter implements ServletContextAttributeListener, ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ContextListenter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+     */
+    public void attributeAdded(ServletContextAttributeEvent scae)  { 
+         // TODO Auto-generated method stub
+    	System.out.println(scae.getName()+"  "+scae.getValue());
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+     */
+    public void attributeRemoved(ServletContextAttributeEvent scae)  { 
+         // TODO Auto-generated method stub
+    	System.out.println(scae.getName()+"  "+scae.getValue());
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("context destroyed");  
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+     */
+    public void attributeReplaced(ServletContextAttributeEvent scae)  { 
+         // TODO Auto-generated method stub
+    	System.out.println(scae.getName()+"  "+scae.getValue());
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("init");
+    }
+	
+}
---
+++ ServletAPI_03/src/com/sullay/Servlet/Test.java
@@ -0,0 +1,38 @@
+package com.sullay.Servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CookieTest
+ */
+@WebServlet("/Test")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletContext servletContext = request.getServletContext();
+		servletContext.setAttribute("aaa", "bbb");
+		servletContext.setAttribute("aaa", "ccc");
+		servletContext.removeAttribute("aaa");
+	}
+
+}
